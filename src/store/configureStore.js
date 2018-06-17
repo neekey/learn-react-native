dbec674a62dc4c74fba7dd9cc33d90034be67e84
neekey <ni184775761@gmail.com>
2018-06-17 23:26:07 +1000

    add redux support for react-native-debugger

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,16 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 import placesReducer from './reducers/places';
 
 const rootReducer = combineReducers({
   places: placesReducer,
 });
 
+let composeEnhancers = compose;
+
+if (__DEV__) {
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+}
+
 export default function configureStore() {
-  return createStore(rootReducer);
+  return createStore(rootReducer, composeEnhancers());
 }
